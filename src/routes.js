af0164af52c207fb11f39a92ced3026bd0307ed0
Zulu Alfa Mike <zamzam.zora@gmail.com>
2023-01-19 20:16:52 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,58 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+  {
+    method: 'POST',
+    path: '/user',
+    handler: (request, h) => {
+      console.log('oke');
+      return h.response('created')
+        .type('text/plain')
+        .header('X-Custom', 'some-value')
+        .code(201);
+    },
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: () => 'Hehehehe...',
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => 'Halaman tidak ditemukan',
+  },
+];
+
+module.exports = routes;
